112cdea7cd357a79eef253783e6e44418289a72b
lintang2209 <nabilalintangadzani@gmail.com>
2025-08-23 12:10:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ elif st.session_state.page == "deteksi":
     def load_cnn_model():
         # --- Unduh model dari Google Drive ---
         GOOGLE_DRIVE_FILE_ID = "1JeSvrid8Zw2xurG-pciDrw6EdI2qXuAd"
-        MODEL_PATH = "models/cnn_soybean_rust.h5"
+        #MODEL_PATH = "models/cnn_soybean_rust.h5"
         
         # Periksa apakah folder "models" ada, jika tidak, buatlah
         os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
